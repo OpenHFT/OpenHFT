207cf63f7fa59e86247baab4c83591f4d5e62728
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2013-07-19 12:37:37 +0100

    Fix bug in position(long) and tune performance tests

--- src/main/java/net/openhft/lang/io/NativeBytes.java
+++ src/main/java/net/openhft/lang/io/NativeBytes.java
@@ -218,7 +218,7 @@ public class NativeBytes extends AbstractBytes {
 
     @Override
     public void writeInt(int v) {
-        UNSAFE.putInt(positionAddr, v);
+        UNSAFE.putInt(null, positionAddr, v);
         positionAddr += 4;
     }
 
