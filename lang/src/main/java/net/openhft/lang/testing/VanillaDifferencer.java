c43aebfa5cebb3dd922f0cfd7d76bf4b8ea83b12
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2013-08-05 19:31:35 +0100

    Add Differencer for calculating differences between machines

---
+++ lang/src/main/java/net/openhft/lang/testing/VanillaDifferencer.java
@@ -0,0 +1,13 @@
+package net.openhft.lang.testing;
+
+/**
+ * User: peter
+ * Date: 05/08/13
+ * Time: 19:07
+ */
+public class VanillaDifferencer implements Differencer {
+    @Override
+    public long sample(long startTime, long endTime) {
+        return endTime - startTime;
+    }
+}
---
+++ lang/src/test/java/net/openhft/lang/testing/RunningMinimumTest.java
@@ -0,0 +1,28 @@
+package net.openhft.lang.testing;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * User: peter
+ * Date: 05/08/13
+ * Time: 19:14
+ */
+public class RunningMinimumTest {
+    @Test
+    public void testSample() throws Exception {
+        for (int k = 0; k < 1000; k++) {
+            for (long delta : new long[]{0, Integer.MIN_VALUE, Integer.MAX_VALUE}) {
+                RunningMinimum rm = new RunningMinimum(50 * 1000);
+                int j;
+                for (j = 0; j < 40 * 1000000; j += 1000000) {
+                    long startTime = System.nanoTime() + j;
+                    long endTime = System.nanoTime() + j + delta + (long) (Math.pow(10 * 1000, Math.random()) * 1000);
+                    rm.sample(startTime, endTime);
+                }
+                assertEquals("delta=" + delta, delta, rm.minimum(), 10 * 1000);
+            }
+        }
+    }
+}
