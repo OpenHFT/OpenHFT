3327a2ea81c3a8f01812d1211991defe958963b1
Peter Lawrey <peter.lawrey@gmail.com>
2013-09-22 18:14:37 -0700

    Add support for raw memory copies of objects

--- lang/src/main/java/net/openhft/lang/io/RawCopier.java
+++ lang/src/main/java/net/openhft/lang/io/RawCopier.java
@@ -24,6 +24,8 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
+import static net.openhft.lang.io.NativeBytes.UNSAFE;
+
 /**
  * User: plawrey
  * Date: 22/09/13
@@ -40,21 +42,29 @@ public class RawCopier<T> {
         Collections.sort(fields, new Comparator<Field>() {
             @Override
             public int compare(Field o1, Field o2) {
-                long off1 = NativeBytes.UNSAFE.objectFieldOffset(o1);
-                long off2 = NativeBytes.UNSAFE.objectFieldOffset(o2);
+                long off1 = UNSAFE.objectFieldOffset(o1);
+                long off2 = UNSAFE.objectFieldOffset(o2);
                 return Double.compare(off1, off2);
             }
         });
-        start = (int) NativeBytes.UNSAFE.objectFieldOffset(fields.get(0));
+        start = (int) UNSAFE.objectFieldOffset(fields.get(0));
         Field lastField = null;
         for (Field field : fields) {
             if (Modifier.isTransient(field.getModifiers()) || !field.getType().isPrimitive())
                 break;
             lastField = field;
         }
-        end = (int) NativeBytes.UNSAFE.objectFieldOffset(lastField) + sizeOf(lastField.getType());
+        end = (int) UNSAFE.objectFieldOffset(lastField) + sizeOf(lastField.getType());
 
         assert end > start : "end <= start, start: " + start + ", end: " + end;
+    }
+
+    public static <T> RawCopier<T> copies(Class<T> tClass) {
+        return new RawCopier<T>(tClass);
+    }
+
+    private static int sizeOf(Class<?> type) {
+        return UNSAFE.arrayIndexScale(Array.newInstance(type, 0).getClass());
     }
 
     public int start() {
@@ -65,20 +75,22 @@ public class RawCopier<T> {
         return end;
     }
 
-    public static <T> RawCopier<T> copies(Class<T> tClass) {
-        return new RawCopier<T>(tClass);
-    }
-
-    private static int sizeOf(Class<?> type) {
-        return NativeBytes.UNSAFE.arrayIndexScale(Array.newInstance(type, 0).getClass());
-    }
-
     public void toBytes(Object obj, Bytes bytes) {
         bytes.writeObject(obj, start, end);
     }
 
     public void fromBytes(Bytes bytes, Object obj) {
         bytes.readObject(obj, start, end);
+    }
+
+    public void copy(T from, T to) {
+        int i;
+        for (i = start; i < end - 7; i += 8) {
+            UNSAFE.putLong(to, i, UNSAFE.getLong(from, i));
+        }
+        for (; i < end; i++) {
+            UNSAFE.putByte(to, i, UNSAFE.getByte(from, i));
+        }
     }
 
     private void addAllFields(List<Field> fields, Class tClass) {
