0735fd1c3d3c914427b2ff6fd33bc98fd80c75fb
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2013-12-26 10:25:46 +0000

    checkout-files

---
+++ checkout.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+git clone git@github.com:OpenHFT/Java-Thread-Affinity.git
+git clone git@github.com:OpenHFT/Java-Lang.git
+git clone git@github.com:OpenHFT/Java-Chronicle.git
+git clone git@github.com:OpenHFT/HugeCollections.git
+git clone git@github.com:OpenHFT/Java-Runtime-Compiler.git
+git clone git@github.com:OpenHFT/TransFIX.git
