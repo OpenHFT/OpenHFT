3b310ad8b58c5f2b64e2ceabbaed9cb655613ab5
lburgazzoli <lburgazzoli@gmail.com>
2014-01-12 18:21:21 +0100

    OSGi : upgrade Pax Exam #16

--- lang-osgi/src/test/java/net/openhft/lang/osgi/OSGiBundleTest.java
+++ lang-osgi/src/test/java/net/openhft/lang/osgi/OSGiBundleTest.java
@@ -16,8 +16,6 @@
 
 package net.openhft.lang.osgi;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Configuration;
@@ -25,7 +23,6 @@ import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
-import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.io.File;
@@ -44,15 +41,19 @@ public class OSGiBundleTest {
 
     @Configuration
     public Option[] config() {
-        Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-        root.setLevel(Level.INFO);
 
         return options(
                 systemProperty("org.osgi.framework.storage.clean").value("true"),
                 systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("WARN"),
+                mavenBundle("org.slf4j","slf4j-api","1.7.5"),
+                mavenBundle("org.slf4j","slf4j-simple","1.7.5").noStart(),
                 mavenBundle("net.openhft", "compiler", "2.1"),
-                new File("Java-Lang/lang/target/classes").exists() ? bundle("reference:file:Java-Lang/lang/target/classes") : bundle("reference:file:../lang/target/classes"),
-                new File("Java-Lang/lang-osgi/target/classes").exists() ? bundle("reference:file:Java-Lang/lang-osgi/target/classes") : bundle("reference:file:target/classes"),
+                new File("Java-Lang/lang/target/classes").exists()
+                    ? bundle("reference:file:Java-Lang/lang/target/classes")
+                    : bundle("reference:file:../lang/target/classes"),
+                new File("Java-Lang/lang-osgi/target/classes").exists()
+                    ? bundle("reference:file:Java-Lang/lang-osgi/target/classes")
+                    : bundle("reference:file:target/classes"),
                 junitBundles(),
                 systemPackage("sun.misc"),
                 systemPackage("sun.nio.ch"),
--- lang-osgi/src/test/java/net/openhft/lang/osgi/OSGiCollectionTest.java
+++ lang-osgi/src/test/java/net/openhft/lang/osgi/OSGiCollectionTest.java
@@ -16,8 +16,6 @@
 
 package net.openhft.lang.osgi;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.Logger;
 import net.openhft.lang.collection.HugeArray;
 import net.openhft.lang.collection.HugeCollections;
 import net.openhft.langosgi.model.JavaBeanInterface;
@@ -28,7 +26,6 @@ import org.ops4j.pax.exam.Configuration;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.osgi.framework.BundleContext;
-import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
 import java.io.File;
@@ -47,15 +44,19 @@ public class OSGiCollectionTest {
 
     @Configuration
     public Option[] config() {
-        Logger root = (Logger) LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME);
-        root.setLevel(Level.INFO);
 
         return options(
                 systemProperty("org.osgi.framework.storage.clean").value("true"),
                 systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level").value("WARN"),
+                mavenBundle("org.slf4j","slf4j-api","1.7.5"),
+                mavenBundle("org.slf4j","slf4j-simple","1.7.5").noStart(),
                 mavenBundle("net.openhft", "compiler", "2.1"),
-                new File("Java-Lang/lang/target/classes").exists() ? bundle("reference:file:Java-Lang/lang/target/classes") : bundle("reference:file:../lang/target/classes"),
-                new File("Java-Lang/lang-osgi/target/classes").exists() ? bundle("reference:file:Java-Lang/lang-osgi/target/classes") : bundle("reference:file:target/classes"),
+                new File("Java-Lang/lang/target/classes").exists()
+                    ? bundle("reference:file:Java-Lang/lang/target/classes")
+                    : bundle("reference:file:../lang/target/classes"),
+                new File("Java-Lang/lang-osgi/target/classes").exists()
+                    ? bundle("reference:file:Java-Lang/lang-osgi/target/classes")
+                    : bundle("reference:file:target/classes"),
                 junitBundles(),
                 systemPackage("sun.misc"),
                 systemPackage("sun.nio.ch"),
