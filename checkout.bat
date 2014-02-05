aed76afc1704fcfa4219430d5a66ffd2a2cab52b
ashelleyhft <anshul.shelley@higherfrequencytrading.com>
2014-02-04 21:03:53 -0500

    Checkout.bat for windows

---
+++ checkout.bat
@@ -0,0 +1,6 @@
+git clone https://github.com/OpenHFT/Java-Thread-Affinity.git
+git clone https://github.com/OpenHFT/Java-Lang.git
+git clone https://github.com/OpenHFT/Java-Chronicle.git
+git clone https://github.com/OpenHFT/HugeCollections.git
+git clone https://github.com/OpenHFT/Java-Runtime-Compiler.git
+git clone https://github.com/OpenHFT/TransFIX.git
