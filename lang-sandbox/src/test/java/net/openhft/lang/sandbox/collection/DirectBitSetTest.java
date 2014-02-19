ea29d02e63b6971e85a9ca77188509db1c09a337
leventov <leventov@ya.ru>
2014-02-19 07:23:45 +0400

    Implemented all the rest methods in ASTDirectBitSet, fixed bugs, added more tests

--- lang-sandbox/src/test/java/net/openhft/lang/sandbox/collection/DirectBitSetTest.java
+++ lang-sandbox/src/test/java/net/openhft/lang/sandbox/collection/DirectBitSetTest.java
@@ -58,10 +58,8 @@ public class DirectBitSetTest {
     }
 
     private void setIndicesComplement() {
-        bs.setAll();
-        for (int i : INDICES) {
-            bs.clear(i);
-        }
+        setIndices();
+        bs.flip(0, bs.size());
     }
 
     @Test
@@ -120,6 +118,23 @@ public class DirectBitSetTest {
     }
 
     @Test
+    public void testNextSetLong() {
+        bs.clear();
+        bs.set(0);
+        bs.set(255);
+        long[] setLongs = {0, 3};
+        int order = 0;
+        for (long i = bs.nextSetLong(0L); i >= 0; i = bs.nextSetLong(i + 1)) {
+            assertEquals(setLongs[order], i);
+            order++;
+        }
+        assertEquals(-1, bs.nextSetLong(bs.size() / 64));
+
+        bs.clear();
+        assertEquals(-1, bs.nextSetLong(0L));
+    }
+
+    @Test
     public void testNextClearBit() {
         setIndicesComplement();
         int order = 0;
@@ -131,6 +146,23 @@ public class DirectBitSetTest {
 
         bs.setAll();
         assertEquals(-1, bs.nextClearBit(0L));
+    }
+
+    @Test
+    public void testNextClearLong() {
+        bs.setAll();
+        bs.clear(0);
+        bs.clear(255);
+        long[] clearLongs = {0, 3};
+        int order = 0;
+        for (long i = bs.nextClearLong(0L); i >= 0; i = bs.nextClearLong(i + 1)) {
+            assertEquals(clearLongs[order], i);
+            order++;
+        }
+        assertEquals(-1, bs.nextClearLong(bs.size() / 64));
+
+        bs.setAll();
+        assertEquals(-1, bs.nextClearLong(0L));
     }
 
     @Test
@@ -148,6 +180,23 @@ public class DirectBitSetTest {
     }
 
     @Test
+    public void testPreviousSetLong() {
+        bs.clear();
+        bs.set(0);
+        bs.set(255);
+        long[] setLongs = {0, 3};
+        int order = setLongs.length;
+        for (long i = bs.size() / 64; (i = bs.previousSetLong(i - 1)) >= 0; ) {
+            order--;
+            assertEquals(setLongs[order], i);
+        }
+        assertEquals(-1, bs.previousSetLong(-1));
+
+        bs.clear();
+        assertEquals(-1, bs.previousSetLong(bs.size() / 64));
+    }
+
+    @Test
     public void testPreviousClearBit() {
         setIndicesComplement();
         int order = INDICES.length;
@@ -160,6 +209,109 @@ public class DirectBitSetTest {
         bs.setAll();
         assertEquals(-1, bs.previousClearBit(bs.size()));
     }
+
+    @Test
+    public void testPreviousClearLong() {
+        bs.setAll();
+        bs.clear(0);
+        bs.clear(255);
+        long[] clearLongs = {0, 3};
+        int order = clearLongs.length;
+        for (long i = bs.size() / 64; (i = bs.previousClearLong(i - 1)) >= 0;) {
+            order--;
+            assertEquals(clearLongs[order], i);
+        }
+        assertEquals(-1, bs.previousClearLong(-1));
+
+        bs.setAll();
+        assertEquals(-1, bs.previousClearLong(bs.size() / 64));
+    }
+
+    @Test
+    public void testSetAll() {
+        bs.clear();
+        bs.setAll();
+        assertEquals(bs.size(), bs.cardinality());
+    }
+
+    @Test
+    public void testRangeOpsWithinLongCase() {
+        bs.clear();
+
+        bs.flip(0, 0);
+        assertEquals(false, bs.get(0));
+        assertEquals(0, bs.cardinality());
+        bs.flip(0, 1);
+        assertEquals(true, bs.get(0));
+        assertEquals(1, bs.cardinality());
+
+        bs.clear(0, 0);
+        assertEquals(true, bs.get(0));
+        assertEquals(1, bs.cardinality());
+        bs.clear(0, 1);
+        assertEquals(false, bs.get(0));
+        assertEquals(0, bs.cardinality());
+
+        bs.set(0, 0);
+        assertEquals(false, bs.get(0));
+        assertEquals(0, bs.cardinality());
+        bs.set(0, 1);
+        assertEquals(true, bs.get(0));
+        assertEquals(1, bs.cardinality());
+    }
+
+    @Test
+    public void testRangeOpsCrossLongCase() {
+        bs.clear();
+
+        bs.flip(63, 64);
+        assertEquals(true, bs.get(63));
+        assertEquals(false, bs.get(64));
+        assertEquals(1, bs.cardinality());
+        bs.flip(63, 65);
+        assertEquals(false, bs.get(63));
+        assertEquals(true, bs.get(64));
+        assertEquals(1, bs.cardinality());
+
+        bs.clear(64);
+        bs.set(63, 64);
+        assertEquals(true, bs.get(63));
+        assertEquals(false, bs.get(64));
+        assertEquals(1, bs.cardinality());
+
+        bs.set(64);
+        bs.clear(63, 64);
+        assertEquals(false, bs.get(63));
+        assertEquals(true, bs.get(64));
+        assertEquals(1, bs.cardinality());
+
+        bs.clear(64);
+        bs.set(63, 65);
+        assertEquals(true, bs.get(63));
+        assertEquals(true, bs.get(64));
+        assertEquals(2, bs.cardinality());
+
+        bs.clear(63, 65);
+        assertEquals(false, bs.get(63));
+        assertEquals(false, bs.get(64));
+        assertEquals(0, bs.cardinality());
+
+    }
+
+    @Test
+    public void testRangeOpsSpanLongCase() {
+        bs.clear();
+
+        bs.set(0, bs.size());
+        assertEquals(bs.size(), bs.cardinality());
+
+        bs.clear(0, bs.size());
+        assertEquals(0, bs.cardinality());
+
+        bs.flip(0, bs.size());
+        assertEquals(bs.size(), bs.cardinality());
+    }
+
 
     @Test(expected = IndexOutOfBoundsException.class)
     public void testIoobeGetNegative() {
@@ -201,9 +353,15 @@ public class DirectBitSetTest {
         bs.getLong((bs.size() + 63) / 64);
     }
 
+
     @Test(expected = IndexOutOfBoundsException.class)
     public void testIoobeNextSetBit() {
         bs.nextSetBit(-1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIoobeNextSetLong() {
+        bs.nextSetLong(-1);
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
@@ -212,12 +370,73 @@ public class DirectBitSetTest {
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
+    public void testIoobeNextClearLong() {
+        bs.nextClearLong(-1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
     public void testIoobePreviousSetBit() {
         bs.previousSetBit(-2);
     }
 
     @Test(expected = IndexOutOfBoundsException.class)
+    public void testIoobePreviousSetLong() {
+        bs.previousSetLong(-2);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
     public void testIoobePreviousClearBit() {
         bs.previousClearBit(-2);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIoobePreviousClearLong() {
+        bs.previousClearLong(-2);
+    }
+
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIoobeSetRangeFromNegative() {
+        bs.set(-1, 0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIoobeSetRangeFromOverTo() {
+        bs.set(1, 0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIoobeSetRangeToOverCapacity() {
+        bs.set(0, bs.size() + 1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIoobeClearRangeFromNegative() {
+        bs.clear(-1, 0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIoobeClearRangeFromOverTo() {
+        bs.clear(1, 0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIoobeClearRangeToOverCapacity() {
+        bs.clear(0, bs.size() + 1);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIoobeFlipRangeFromNegative() {
+        bs.flip(-1, 0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIoobeFlipRangeFromOverTo() {
+        bs.flip(1, 0);
+    }
+
+    @Test(expected = IndexOutOfBoundsException.class)
+    public void testIoobeFlipRangeToOverCapacity() {
+        bs.flip(0, bs.size() + 1);
     }
 }
