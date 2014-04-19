6108dab544085cd8d096f84810cfcc27a2920b81
Rob Austin <rob.austin@boundedbuffer.co.uk>
2014-04-19 19:46:20 +0100

    Update checkout.sh

--- checkout.sh
+++ checkout.sh
@@ -5,3 +5,4 @@ git clone git@github.com:OpenHFT/Java-Chronicle.git
 git clone git@github.com:OpenHFT/HugeCollections.git
 git clone git@github.com:OpenHFT/Java-Runtime-Compiler.git
 git clone git@github.com:OpenHFT/TransFIX.git
+git clone git@github.com:OpenHFT/SAXophone.git
