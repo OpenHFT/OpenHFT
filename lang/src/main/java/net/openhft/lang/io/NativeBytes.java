d0734c14a01a841f3bcb41b3d42b2fe68a05fc0e
leventov <leventov@ya.ru>
2014-06-01 20:17:41 +0400

    Renewed DirectStore.resize()

--- lang/src/main/java/net/openhft/lang/io/NativeBytes.java
+++ lang/src/main/java/net/openhft/lang/io/NativeBytes.java
@@ -55,7 +55,7 @@ public class NativeBytes extends AbstractBytes {
     protected long startAddr;
     protected long positionAddr;
     protected long limitAddr;
-    long capacityAddr;
+    protected long capacityAddr;
 
     public NativeBytes(long startAddr, long capacityAddr) {
         super();
