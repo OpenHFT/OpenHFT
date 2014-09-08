29e78d85245258b71eefe2bf80bb4a701cecd395
Rob Austin <rob.austin@boundedbuffer.co.uk>
2014-09-08 09:27:11 +0100

    Update checkout.sh
    added Chronicle-Map.git

--- checkout.sh
+++ checkout.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 git clone git@github.com:OpenHFT/Java-Thread-Affinity.git
 git clone git@github.com:OpenHFT/Java-Lang.git
-git clone git@github.com:OpenHFT/Java-Chronicle.git
+git clone git@github.com:OpenHFT/Chronicle-Queue.git
+git clone git@github.com:OpenHFT/Chronicle-Map.git
 git clone git@github.com:OpenHFT/HugeCollections.git
 git clone git@github.com:OpenHFT/Java-Runtime-Compiler.git
 git clone git@github.com:OpenHFT/TransFIX.git
