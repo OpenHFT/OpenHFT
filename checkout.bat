34d547e39a78e07e071ca5c7d21a3d77f4abb81d
Rob Austin <rob.austin@boundedbuffer.co.uk>
2014-09-08 09:28:59 +0100

    Update checkout.bat

--- checkout.bat
+++ checkout.bat
@@ -1,6 +1,7 @@
 git clone https://github.com/OpenHFT/Java-Thread-Affinity.git
 git clone https://github.com/OpenHFT/Java-Lang.git
-git clone https://github.com/OpenHFT/Java-Chronicle.git
+git clone https://github.com/OpenHFT/Chronicle-Queue.git
+git clone https://github.com/OpenHFT/Chronicle-Map.git
 git clone https://github.com/OpenHFT/HugeCollections.git
 git clone https://github.com/OpenHFT/Java-Runtime-Compiler.git
 git clone https://github.com/OpenHFT/TransFIX.git
