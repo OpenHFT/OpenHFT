edfce14490a99f2749ff3c22e468708460bc5a40
Rob Austin <rob.austin@boundedbuffer>
2014-09-09 11:10:01 +0100

    added  Chronicle-Logger

--- checkout.bat
+++ checkout.bat
@@ -6,3 +6,4 @@ git clone https://github.com/OpenHFT/HugeCollections.git
 git clone https://github.com/OpenHFT/Java-Runtime-Compiler.git
 git clone https://github.com/OpenHFT/TransFIX.git
 git clone https://github.com/OpenHFT/SAXophone.git
+git clone git@github.com:OpenHFT/Chronicle-Logger.git
