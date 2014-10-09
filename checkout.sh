70099176ca190738b413adf77aaddd8e51fbf9c8
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2014-10-09 13:36:07 +0100

    Some javadoc migrated to Java 8.

--- checkout.sh
+++ checkout.sh
@@ -3,8 +3,8 @@ git clone git@github.com:OpenHFT/Java-Thread-Affinity.git
 git clone git@github.com:OpenHFT/Java-Lang.git
 git clone git@github.com:OpenHFT/Chronicle-Queue.git
 git clone git@github.com:OpenHFT/Chronicle-Map.git
-git clone git@github.com:OpenHFT/HugeCollections.git
 git clone git@github.com:OpenHFT/Chronicle-Logger.git
+git clone git@github.com:OpenHFT/Chronucle-Engine.git
 git clone git@github.com:OpenHFT/Java-Runtime-Compiler.git
 git clone git@github.com:OpenHFT/TransFIX.git
 git clone git@github.com:OpenHFT/SAXophone.git
