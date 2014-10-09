e4c397869e9525c04d6c32eab934866e47674de6
Rob Austin <rob.austin@boundedbuffer>
2014-10-09 17:50:20 +0100

    moved set to ChronicleMap

--- checkout.bat
+++ checkout.bat
@@ -2,7 +2,7 @@ git clone https://github.com/OpenHFT/Java-Thread-Affinity.git
 git clone https://github.com/OpenHFT/Java-Lang.git
 git clone https://github.com/OpenHFT/Chronicle-Queue.git
 git clone https://github.com/OpenHFT/Chronicle-Map.git
-git clone https://github.com/OpenHFT/Chronicle-Egine.git
+git clone https://github.com/OpenHFT/Chronicle-Engine.git
 git clone https://github.com/OpenHFT/Java-Runtime-Compiler.git
 git clone https://github.com/OpenHFT/TransFIX.git
 git clone https://github.com/OpenHFT/SAXophone.git
