b4fa2e8198c1280448cdb2fb820e3d401200a9cb
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2014-11-04 04:25:49 +0000

    HCOLL-171 Move default BytesHasher to java-lang.

---
+++ lang/src/main/java/net/openhft/lang/io/VanillaBytesHasher.java
@@ -0,0 +1,28 @@
+package net.openhft.lang.io;
+
+import net.openhft.lang.Maths;
+
+public enum VanillaBytesHasher implements BytesHasher {
+    INSTANCE;
+    private static final long LONG_LEVEL_PRIME_MULTIPLE = 0x9ddfea08eb382d69L;
+    private static final short SHORT_LEVEL_PRIME_MULTIPLE = 0x404f;
+    private static final byte BYTE_LEVEL_PRIME_MULTIPLE = 0x57;
+
+    public long hash(Bytes bytes) {
+        return hash(bytes, bytes.position(), bytes.limit());
+    }
+
+    public long hash(Bytes bytes, long offset, long limit) {
+        if (limit - offset == 8)
+            return Maths.hash(bytes.readLong(offset));
+        long h = 0;
+        long i = offset;
+        for (; i < limit - 7; i += 8)
+            h = LONG_LEVEL_PRIME_MULTIPLE * h + bytes.readLong(i);
+        for (; i < limit - 1; i += 2)
+            h = SHORT_LEVEL_PRIME_MULTIPLE * h + bytes.readShort(i);
+        if (i < limit)
+            h = BYTE_LEVEL_PRIME_MULTIPLE * h + bytes.readByte(i);
+        return Maths.hash(h);
+    }
+}
