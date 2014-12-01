dfb0237e75ac34f120602ef7016de7e4a05b652b
Rob Austin <rob.austin@boundedbuffer.co.uk>
2014-12-01 19:49:19 +0000

    categorised @ignore with JIRA's  and fixed test cases and List Marshaller

--- lang/src/main/java/net/openhft/lang/io/serialization/impl/CollectionMarshaller.java
+++ lang/src/main/java/net/openhft/lang/io/serialization/impl/CollectionMarshaller.java
@@ -34,6 +34,8 @@ abstract class CollectionMarshaller<E, C extends Collection<E>> {
 
     public C read(Bytes bytes, @Nullable C c) {
         long length = bytes.readStopBit();
+        if (length == 0 && c != null)
+            return c;
         if (length < NULL_LENGTH || length > Integer.MAX_VALUE)
             throw new IllegalStateException("Invalid length: " + length);
 
--- lang/src/main/java/net/openhft/lang/io/serialization/impl/ListMarshaller.java
+++ lang/src/main/java/net/openhft/lang/io/serialization/impl/ListMarshaller.java
@@ -5,7 +5,6 @@ import net.openhft.lang.io.serialization.BytesMarshaller;
 import net.openhft.lang.io.serialization.CompactBytesMarshaller;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class ListMarshaller<E> extends CollectionMarshaller<E, List<E>> implements CompactBytesMarshaller<List<E>> {
@@ -30,21 +29,13 @@ public class ListMarshaller<E> extends CollectionMarshaller<E, List<E>> implemen
 
     @Override
     List<E> readCollection(Bytes bytes, List<E> es, int length) {
-        List<E> ret = newCollection();
-        if (es == null || (es.size() != length)) {
-            if (es == null)
-                es = Collections.emptyList();
-            List<E> es2 = newCollection();
-            for (int i = 0; i < length; i++)
-                if (i < es.size()) {
-                    ret.add(eBytesMarshaller.read(bytes, es.get(i)));
-                } else {
-                    ret.add(eBytesMarshaller.read(bytes));
-                }
-            return es2;
+        List<E> ret = es;
+        ret.clear();
+
+        for (int i = 0; i < length; i++) {
+            ret.add(eBytesMarshaller.read(bytes));
         }
-        for (int i = 0; i < length; i++)
-            eBytesMarshaller.read(bytes, es.get(i));
+
         return ret;
     }
 }
