b701b7ba3c974fe4b0a818afce99e05a2a8ff62f
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-01-15 17:35:44 +0000

    Use ChronicleMapBuilder to build all ChronicleMap

--- lang-sandbox/src/main/java/net/openhft/lang/data/ByamlWire.java
+++
@@ -1,9 +0,0 @@
-package net.openhft.lang.data;
-
-/**
- * This is a binary form of YAML which could be smaller, but should be faster to write and parse.
- * *
- * Created by peter on 12/01/15.
- */
-public class ByamlWire implements Wire {
-}
