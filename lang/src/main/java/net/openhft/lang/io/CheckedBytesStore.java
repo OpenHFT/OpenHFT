bd03b978ee74b2c5f834f83993e93f7273dd250e
Rob Austin <rob.austin@boundedbuffer.co.uk>
2015-02-01 19:29:20 +0000

    added class that checks if the buffer has been closed, if the buffer is accessed after it is closed an exception is thrown, This class should be used to investigate JVM crashes, it should not be used in production code ( as its slow )

---
+++ lang/src/main/java/net/openhft/lang/io/CheckedBytesStore.java
@@ -0,0 +1,98 @@
+package net.openhft.lang.io;
+
+import net.openhft.lang.io.serialization.ObjectSerializer;
+
+import java.io.File;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Checks if the buffer has been closed, if the buffer is accessed after it is closed an exception is thrown.
+ * This class should be used to investigate JVM crashes, it should not be used in production code ( as its slow )
+ *
+ * Created by Rob Austin
+ */
+public class CheckedBytesStore implements BytesStore {
+
+    private final BytesStore bytesStore;
+    private AtomicBoolean isClosed = new AtomicBoolean();
+
+    Bytes proxy;
+
+    public CheckedBytesStore(final BytesStore bytesStore) {
+        this.bytesStore = bytesStore;
+        InvocationHandler handler = new InvocationHandler() {
+
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                if (isClosed.get())
+                    throw new IllegalStateException(Thread.currentThread().getName() + " calling " + method + " after the byteStore has been freed.");
+                return method.invoke(bytesStore.bytes(), args);
+
+            }
+        };
+
+        proxy = (Bytes) Proxy.newProxyInstance(
+                Bytes.class.getClassLoader(),
+                new Class[]{Bytes.class},
+                handler);
+    }
+
+    @Override
+    public Bytes bytes() {
+        return proxy;
+    }
+
+    @Override
+    public Bytes bytes(final long offset, final long length) {
+
+        InvocationHandler handler = new InvocationHandler() {
+            Bytes bytes0 = bytesStore.bytes(offset, length);
+
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                if (isClosed.get())
+                    throw new IllegalStateException(Thread.currentThread().getName() + " calling " + method + " after the byteStore has been freed.");
+                return method.invoke(bytes0, args);
+            }
+        };
+
+        return (Bytes) Proxy.newProxyInstance(
+                Bytes.class.getClassLoader(),
+                new Class[]{Bytes.class},
+                handler);
+    }
+
+    @Override
+    public long address() {
+        if (isClosed.get())
+            throw new IllegalStateException(Thread.currentThread().getName() + " called after the byteStore has been freed.");
+
+        return bytesStore.address();
+    }
+
+    @Override
+    public long size() {
+        if (isClosed.get())
+            throw new IllegalStateException(Thread.currentThread().getName() + " called after the byteStore has been freed.");
+
+        return bytesStore.size();
+    }
+
+    @Override
+    public void free() {
+        isClosed.set(true);
+    }
+
+    @Override
+    public ObjectSerializer objectSerializer() {
+        return bytesStore.objectSerializer();
+    }
+
+    @Override
+    public File file() {
+        return bytesStore.file();
+    }
+}
