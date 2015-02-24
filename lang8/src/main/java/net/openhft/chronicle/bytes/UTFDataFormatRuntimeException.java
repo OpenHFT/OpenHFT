1531cd5dbda7a4dd4f3b87062ecf9adb2be44926
Rob Austin <rob.austin@boundedbuffer.co.uk>
2015-02-24 14:04:25 +0000

    added chronicle-core

--- lang8/src/main/java/net/openhft/chronicle/bytes/UTFDataFormatRuntimeException.java
+++
@@ -1,7 +0,0 @@
-package net.openhft.chronicle.bytes;
-
-public class UTFDataFormatRuntimeException extends IORuntimeException {
-    public UTFDataFormatRuntimeException(String message) {
-        super(message);
-    }
-}
--- lang8/src/main/java/net/openhft/chronicle/core/Threads.java
+++
@@ -1,9 +0,0 @@
-package net.openhft.chronicle.core;
-
-public enum Threads {
-    ;
-
-    public static void checkInterrupted() {
-        throw new UnsupportedOperationException();
-    }
-}
