383f8820d9455806b36e33f7e7a3a23d4cc378e7
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-03-03 22:54:04 +0000

    Rename peter and peter_2 to peter.lawrey

--- checkout.sh
+++ checkout.sh
@@ -5,13 +5,13 @@ git clone git@github.com:OpenHFT/Chronicle-Core.git
 git clone git@github.com:OpenHFT/Chronicle-Bytes.git
 git clone git@github.com:OpenHFT/Chronicle-Values.git
 git clone git@github.com:OpenHFT/Chronicle-Threads.git
+git clone git@github.com:OpenHFT/Chronicle-Wire.git
+git clone git@github.com:OpenHFT/Chronicle-Network.git
 git clone git@github.com:OpenHFT/Chronicle-Queue.git
 git clone git@github.com:OpenHFT/Chronicle-Map.git
 git clone git@github.com:OpenHFT/Chronicle-Map-Java8.git
 git clone git@github.com:OpenHFT/Chronicle-Logger.git
 git clone git@github.com:OpenHFT/Chronicle-Engine.git
-git clone git@github.com:OpenHFT/Chronicle-Wire.git
-git clone git@github.com:OpenHFT/Chronicle-Network.git
 git clone git@github.com:OpenHFT/Java-Runtime-Compiler.git
 git clone git@github.com:OpenHFT/TransFIX.git
 git clone git@github.com:OpenHFT/SAXophone.git
