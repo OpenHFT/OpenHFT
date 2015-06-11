9d32cd453cba1f2b076f0b02fb8bec03cb79f74d
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-06-11 17:54:20 +0100

    Rename MapEvent to ChangeEvent to make it more generic

--- lang/src/main/java/net/openhft/lang/Jvm.java
+++ lang/src/main/java/net/openhft/lang/Jvm.java
@@ -18,16 +18,12 @@
 
 package net.openhft.lang;
 
-import net.openhft.lang.io.NativeBytes;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.management.ManagementFactory;
-import java.lang.reflect.Field;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -37,20 +33,6 @@ import java.util.logging.Logger;
 public enum Jvm {
     ;
 
-    static {
-        boolean success = false;
-        try {
-
-            Field vm_supports_long_cas = AtomicLong.class.getDeclaredField("VM_SUPPORTS_LONG_CAS");
-            vm_supports_long_cas.setAccessible(true);
-            success = (Boolean) vm_supports_long_cas.get(null);
-        } catch (Exception e) {
-            // do nothing
-        }
-        VM_SUPPORTS_LONG_CAS = success;
-    }
-
-    public static final String TMP = System.getProperty("java.io.tmpdir");
     private static final boolean IS64BIT = is64Bit0();
 
     public static boolean is64Bit() {
@@ -70,8 +52,6 @@ public enum Jvm {
         systemProp = System.getProperty("java.vm.version");
         return systemProp != null && systemProp.contains("_64");
     }
-
-    private final static boolean VM_SUPPORTS_LONG_CAS;
 
     private static final int PROCESS_ID = getProcessId0();
 
@@ -102,10 +82,6 @@ public enum Jvm {
         } else {
             return Integer.parseInt(pid);
         }
-    }
-
-    public static boolean vmSupportsCS8() {
-        return VM_SUPPORTS_LONG_CAS;
     }
 
     /**
@@ -169,92 +145,7 @@ public enum Jvm {
         return 1L << 16;
     }
 
-    private static String convertStreamToString(java.io.InputStream is) {
-        java.util.Scanner s = new java.util.Scanner(is).useDelimiter("\\A");
-        return s.hasNext() ? s.next() : "";
-    }
-
-    public static long freePhysicalMemoryOnWindowsInBytes() throws IOException {
-        if (!isWindows()) {
-            throw new IllegalStateException("Method freePhysicalMemoryOnWindowsInBytes() should " +
-                    "be called only on windows. Use Jvm.isWindows() to check the OS.");
-        }
-
-        Process pr = Runtime.getRuntime().exec("wmic OS get FreePhysicalMemory /Value");
-        try {
-            int result = pr.waitFor();
-            String output = convertStreamToString(pr.getInputStream());
-            if (result != 0) {
-                String errorOutput = convertStreamToString(pr.getErrorStream());
-                throw new IOException("Couldn't get free physical memory on windows. " +
-                        "Command \"wmic OS get FreePhysicalMemory /Value\" exited with " +
-                        result + " code, output: \"" + output + "\", error output: \"" +
-                        errorOutput + "\"");
-            }
-            String[] parts = output.trim().split("=");
-            if (parts.length != 2) {
-                throw new IOException("Couldn't get free physical memory on windows. " +
-                        "Command \"wmic OS get FreePhysicalMemory /Value\" output has unexpected " +
-                        "format: \"" + output + "\"");
-            }
-            try {
-                return MemoryUnit.KILOBYTES.toBytes(Long.parseLong(parts[1]));
-            } catch (NumberFormatException e) {
-                throw new IOException("Couldn't get free physical memory on windows. " +
-                        "Command \"wmic OS get FreePhysicalMemory /Value\" output has unexpected " +
-                        "format: \"" + output + "\"", e);
-            }
-        } catch (InterruptedException e) {
-            throw new IOException(e);
-        }
-    }
-
-    public static void checkInterrupted() {
-        if (Thread.currentThread().isInterrupted()) throw new InterruptedRuntimeException();
-    }
-
-    /**
-     * Utility method to support throwing checked exceptions out of the streams API
-     *
-     * @param t the exception to rethrow
-     * @return the exception
-     */
-    public static RuntimeException rethrow(Throwable t) {
-        NativeBytes.UNSAFE.throwException(t);
-        throw new AssertionError();
-    }
-
     static class LoggerHolder {
         public static final Logger LOGGER = Logger.getLogger(Jvm.class.getName());
-    }
-
-    public static void trimStackTrace(StringBuilder sb, StackTraceElement... stes) {
-        int first = trimFirst(stes);
-        int last = trimLast(first, stes);
-        for (int i = first; i <= last; i++)
-            sb.append("\n\tat ").append(stes[i]);
-    }
-
-    private static int trimFirst(StackTraceElement[] stes) {
-        int first = 0;
-        for (; first < stes.length; first++)
-            if (!isInternal(stes[first].getClassName()))
-                break;
-        if (first > 0) first--;
-        if (first > 0) first--;
-        return first;
-    }
-
-    private static int trimLast(int first, StackTraceElement[] stes) {
-        int last = stes.length - 1;
-        for (; first < last; last--)
-            if (!isInternal(stes[last].getClassName()))
-                break;
-        if (last < stes.length - 1) last++;
-        return last;
-    }
-
-    public static boolean isInternal(String className) {
-        return className.startsWith("jdk.") || className.startsWith("sun.") || className.startsWith("java.");
     }
 }
