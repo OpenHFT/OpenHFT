7f60e4d1668ff8ab9982f3bb07ca0edc9d47eb2c
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2015-12-03 20:18:00 +0000

    Add FX example

--- checkout.sh
+++ checkout.sh
@@ -15,6 +15,6 @@ git clone git@github.com:OpenHFT/Chronicle-Map-Java8.git
 git clone git@github.com:OpenHFT/Chronicle-Logger.git
 git clone git@github.com:OpenHFT/Chronicle-Engine.git
 git clone git@github.com:OpenHFT/Java-Runtime-Compiler.git
-git clone git@github.com:OpenHFT/TransFIX.git
-git clone git@github.com:OpenHFT/SAXophone.git
+#git clone git@github.com:OpenHFT/TransFIX.git
+#git clone git@github.com:OpenHFT/SAXophone.git
 #git clone git@github.com:OpenHFT/YCSB.git
