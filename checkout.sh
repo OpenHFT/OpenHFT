23d65b205688f1626ee12d4963cb2ae55db5e1df
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2017-06-29 12:21:56 +0100

    Fixes to text support for MethodReader

--- checkout.sh
+++ checkout.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 git clone git@github.com:OpenHFT/RFC.git
 git clone git@github.com:OpenHFT/Java-Thread-Affinity.git
-git clone git@github.com:OpenHFT/Java-Lang.git
+#git clone git@github.com:OpenHFT/Java-Lang.git
 git clone git@github.com:OpenHFT/Chronicle-Core.git
-git clone git@github.com:OpenHFT/Chronicle-Algorithms.git
+#git clone git@github.com:OpenHFT/Chronicle-Algorithms.git
 git clone git@github.com:OpenHFT/Chronicle-Bytes.git
 git clone git@github.com:OpenHFT/Chronicle-Values.git
 git clone git@github.com:OpenHFT/Chronicle-Threads.git
@@ -12,10 +12,7 @@ git clone git@github.com:OpenHFT/Chronicle-Network.git
 git clone git@github.com:OpenHFT/Chronicle-Websocket-Jetty.git
 git clone git@github.com:OpenHFT/Chronicle-Queue.git
 git clone git@github.com:OpenHFT/Chronicle-Map.git
-git clone git@github.com:OpenHFT/Chronicle-Map-Java8.git
+#git clone git@github.com:OpenHFT/Chronicle-Map-Java8.git
 git clone git@github.com:OpenHFT/Chronicle-Logger.git
 git clone git@github.com:OpenHFT/Chronicle-Engine.git
 git clone git@github.com:OpenHFT/Java-Runtime-Compiler.git
-#git clone git@github.com:OpenHFT/TransFIX.git
-#git clone git@github.com:OpenHFT/SAXophone.git
-#git clone git@github.com:OpenHFT/YCSB.git
