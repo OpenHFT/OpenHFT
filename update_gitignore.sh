6046e261c0ad082ef515cefc24e0f5c3df50b5f3
Peter Lawrey <peter.lawrey@higherfrequencytrading.com>
2018-01-21 10:08:04 +0000

    Standardise .gitignore across projects

---
+++ update_gitignore.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+cp .gitignore Chronicle-Bytes/
+cp .gitignore Chronicle-Core
+cp .gitignore Chronicle-Engine/
+cat .gitignore Chronicle-FIX/.gitignore.added > Chronicle-FIX/.gitignore
+cp .gitignore Chronicle-Logger/
+cp .gitignore Chronicle-Map/
+cp .gitignore Chronicle-Network/
+cp .gitignore Chronicle-Queue/
+cp .gitignore Chronicle-Salt/
+cp .gitignore Chronicle-Threads/
+cp .gitignore Chronicle-Values/
+cp .gitignore Chronicle-Websocket-Jetty/
+cp .gitignore Chronicle-Wire/
+cp .gitignore Java-Runtime-Compiler/
+cp .gitignore Java-Thread-Affinity/
